d066fad0c379db2e23cb439ee76c3929ab33f5da
Lysenko <lysenkoden@gmail.com>
2023-10-17 23:17:25 +0200

    finish logic and edit adaptive

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // https://fakestoreapi.com/
 
 //* 1. fetch list of items to sale
-// 2. logic to cart:
-// 2.1. add item, price, quality
-// 2.2. add listener to the button on every item with function add to cart (every time it pushed item++)
-// 2.3. add items properties to the cart
-// 2.4. add items quantity counter to cart (with change quantity ability)
+//* 2. logic to cart:
+//* 2.1. add item, price, quality
+//* 2.2. add listener to the button on every item with function add to cart
+//* 2.3. add items properties to the cart
+//* 2.4. add items quantity counter to cart (with change quantity ability)
 //* 2.5. add remove button to every item of cart
 //* 2.6. add total to cart
-// 2.7. if cart is empty by removing all of items --> 'cart is empty'
 
 const url = "https://fakestoreapi.com/products?limit=9";
 
@@ -18,73 +17,15 @@ async function getData(url) {
   return json;
 }
 
-const cartsUl = document.querySelector(".cart-list");
+const cartsUlNode = document.querySelector(".cart-list");
+const cartEmptyTitleNode = document.querySelector(".cart-empty-title");
 const cartSection = document.querySelector(".cart-list-titles");
 const cartTotal = document.querySelector(".total-amount");
-const TotalSpanNode = document.querySelector("span");
+const totalSpanNode = document.querySelector("span");
 cartSection.style.display = "none";
 cartTotal.style.display = "none";
 let arrCart = [];
 let counter = 1;
-
-function updateCartDisplay() {
-  if (arrCart.length > 0) {
-    cartSection.style.display = "flex";
-    cartTotal.style.display = "block";
-    if (cartsUl.hasChildNodes()) {
-      cartsUl.innerHTML = "";
-    }
-    arrCart.map((element) => {
-      cartsUl.insertAdjacentHTML(
-        "beforeend",
-        `<li class='item-cart' id=${
-          element.id
-        }><div class='li-cart-wrapper'><div class='li-cart-img-title'><img src=${
-          element.image
-        } alt='item' class=img-cart><h2 class='title-cart'>${
-          element.title
-        }</h2></div><p class='price-cart'>${new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format(
-          element.price
-        )}</p><div class='li-cart-qty-btn'><input type='number' id='qty' value="${counter}"><button class='button-cart'  onclick="removeFromChartHandler(${
-          element.id
-        })">Remove</button></div></div></li>`
-      );
-    });
-  }
-}
-
-function addToChartHandler(element) {
-  //!working================================================================
-  if (arrCart.find((elementArr) => elementArr.id === element.id)) {
-    counter = ++counter;
-    return;
-  }
-  //!working================================================================
-  arrCart.push({
-    id: element.id,
-    image: element.image,
-    title: element.title,
-    price: element.price,
-    counter: element.counter,
-  });
-  updateCartDisplay();
-}
-
-function removeFromChartHandler(id) {
-  const arrFiltered = arrCart.filter((element) => element.id !== id);
-  arrCart = arrFiltered;
-  console.log(arrCart); //
-  updateCartDisplay();
-
-  if (arrCart.length == 0) {
-    cartsUl.innerHTML = "";
-    cartSection.style.display = "none";
-    cartTotal.style.display = "none";
-  }
-}
 
 async function setData() {
   try {
@@ -107,25 +48,101 @@ async function setData() {
           }
         ).format(element.price)}</span><button class='button-box' id=${
           element.id
-        } onclick="addToChartHandler(${JSON.stringify(element)
+        } onclick="addToCartHandler(${JSON.stringify(element)
           .split('"')
           .join("&quot;")})">Add to cart</button></div></div></li>`
       );
     });
-    //Total======================================================
-    function getTotalCosts(arrCart) {
-      const arrNew = arrCart.map((item) => {
-        return item.price * item.counter;
-      });
-      console.log(arrNew); //
-    }
-    console.log(arrCart); //
-    getTotalCosts(arrCart);
-    TotalSpanNode.innerText = "more";
-    //Total======================================================
   } catch (error) {
     console.log(error.message); //
   }
 }
-
 setData();
+
+function updateCartDisplay() {
+  if (arrCart.length > 0) {
+    cartEmptyTitleNode.style.display = "none";
+    cartSection.style.display = "flex";
+    cartTotal.style.display = "block";
+
+    if (cartsUlNode.hasChildNodes()) {
+      cartsUlNode.innerHTML = "";
+    }
+    arrCart.map((element) => {
+      cartsUlNode.insertAdjacentHTML(
+        "beforeend",
+        `<li class='item-cart' id=${
+          element.id
+        }><div class='li-cart-wrapper'><div class='li-cart-img-title'><img src=${
+          element.image
+        } alt='item' class=img-cart><h2 class='title-cart'>${
+          element.title
+        }</h2></div><p class='price-cart'>${new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(
+          element.price
+        )}</p><div class='li-cart-qty-btn'><input type='number' class= 'li-cart-qty-input' id='qty${
+          element.id
+        }' onchange="changeInputHandler(${element.id})" value="${
+          element.counter
+        }" min="1"><button class='button-cart'  onclick="removeFromCartHandler(${
+          element.id
+        })">Remove</button></div></div></li>`
+      );
+    });
+  }
+}
+
+function addToCartHandler(element) {
+  //if we pick the same item multiple times:
+  if (arrCart.find((elementArr) => elementArr.id === element.id)) {
+    arrCart.find((elementArr) => elementArr.id === element.id).counter++;
+    updateCartDisplay();
+    getTotalCosts(arrCart);
+    return;
+  }
+
+  arrCart.push({
+    id: element.id,
+    image: element.image,
+    title: element.title,
+    price: element.price,
+    counter: counter,
+  });
+  updateCartDisplay();
+  getTotalCosts(arrCart);
+}
+
+function removeFromCartHandler(id) {
+  const arrFiltered = arrCart.filter((element) => element.id !== id);
+  arrCart = arrFiltered;
+  updateCartDisplay();
+  getTotalCosts(arrCart);
+  if (arrCart.length == 0) {
+    cartsUlNode.innerHTML = "";
+    cartEmptyTitleNode.style.display = "block";
+    cartSection.style.display = "none";
+    cartTotal.style.display = "none";
+  }
+}
+
+function changeInputHandler(id) {
+  const idForNode = `qty${id}`;
+  const cartInputNode = document.getElementById(idForNode);
+  arrCart.find((elementArr) => elementArr.id === id).counter =
+    cartInputNode.value;
+  updateCartDisplay();
+  getTotalCosts(arrCart);
+}
+
+function getTotalCosts(arrCart) {
+  const numTotalSum = arrCart.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.counter * currentValue.price;
+  }, 0);
+
+  totalSpanNode.innerText = `${new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(numTotalSum)}`;
+}
